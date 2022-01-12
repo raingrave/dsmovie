c4ca5a160f84a683bfbec0e41a9d5b3132add13c
Luis Fernando <raintech@outlook.com>
2022-01-12 01:13:38 -0300

    added MovieStars component

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css'
+import { ReactComponent as StarFull } from 'assets/images/star-full.svg';
+import { ReactComponent as StarHalf } from 'assets/images/star-half.svg';
+import { ReactComponent as StarEmpty } from 'assets/images/star-empty.svg';
+
+function MovieStars() {
+  return (
+    <div className="dsmovie-stars-container">
+        <StarFull />
+        <StarFull />
+        <StarFull />
+        <StarHalf />
+        <StarEmpty />
+    </div>
+  );
+}
+
+export default MovieStars;
